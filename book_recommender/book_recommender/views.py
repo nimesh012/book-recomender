2df7a8da40b6a8f0ac0577b0cd44fad7c9223af9
nimesh012 <cnimesh1412@gmail.com>
2023-01-16 03:33:44 +0530

    top 50(done)

--- book_recommender/book_recommender/views.py
+++ book_recommender/book_recommender/views.py
@@ -5,7 +5,19 @@ import pandas as pd
 import requests
 from random import sample
 
+popular_df = pd.read_csv('book_recommender\popular.csv')
 
+
+
+
+def popular(df):
+    li = []
+    for i in range(0, df.shape[0]):
+        data = {'name': df['Book-Title'][i], 'author': df['Book-Author'][i], 'rating': round(df['Avg-rating'][i], 2),
+                'img': df['Image-URL-L'][i]}
+        li.append(data)
+
+    return li
 
 
 def index(request):
@@ -13,4 +25,6 @@ def index(request):
 
 
 def top(request):
-    return render(request, 'top.html')
+    top_data = popular(popular_df)
+    params = {'data': top_data}
+    return render(request, 'top.html', params)
