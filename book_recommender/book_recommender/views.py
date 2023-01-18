accafd885f9f3c24d0a6d143aedb8d63bde6af31
nimesh012 <cnimesh1412@gmail.com>
2023-01-18 20:39:13 +0530

    done!

--- book_recommender/book_recommender/views.py
+++ book_recommender/book_recommender/views.py
@@ -2,12 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import pickle
 import pandas as pd
+import numpy as np
 import requests
 from random import sample
 
-popular_df = pd.read_csv('book_recommender\popular.csv')
-
-
+popular_df = pd.read_csv('book_recommender\\popular.csv')
+books = pd.read_csv('book_recommender\\books.csv')
+pt = pickle.load(open('pt.pkl', 'rb'))
+cs = pickle.load(open('cs.pkl', 'rb'))
 
 
 def popular(df):
@@ -34,6 +36,35 @@ def recommend(request):
     return render(request, 'recommend.html')
 
 
-def recommendation(request):
+def recommend_book(book_name):
+    pointer = False
 
-    return render(request, 'recommendation.html')
+    book_d = []
+    if book_name not in pt.index:
+        for i in range(0, popular_df.shape[0]):
+            data = {'title': popular_df['Book-Title'][i], 'author': popular_df['Book-Author'][i], 'year': round(popular_df['Avg-rating'][i], 2),
+                    'Image': popular_df['Image-URL-L'][i]}
+            book_d.append(data)
+        return book_d, pointer
+
+    else:
+        pointer = True
+        book_data = []
+        index_t = np.where(pt.index == book_name)[0][0]
+        similar = sorted(list(enumerate(cs[index_t])), key=lambda x: x[1], reverse=True)[1:7]
+        for i in similar:
+            data = {'title': list(books[books['Book-Title'] == pt.index[i[0]]]['Book-Title'].values)[0],
+                    'author': list(books[books['Book-Title'] == pt.index[i[0]]]['Book-Author'].values)[0],
+                    'Year': list(books[books['Book-Title'] == pt.index[i[0]]]['Year-Of-Publication'].values)[0],
+                    'Image': list(books[books['Book-Title'] == pt.index[i[0]]]['Image-URL-L'].values)[0]}
+            book_data.append(data)
+        return book_data, pointer
+
+
+
+
+def recommendation(request):
+    book_recommended = request.GET.get('book', 'default')
+    book_list, pointer = recommend_book(book_recommended)
+    params = {'books_list': book_list, 'pointer': pointer}
+    return render(request, 'recommendation.html', params)
